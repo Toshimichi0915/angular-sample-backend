e450390d737af01d721f3c8f8ee4fec459f480cd
Toshimichi0915 <main@toshimichi.net>
2022-06-03 09:53:18 +0900

    Add non-working code

---
+++ bin/main/net/toshimichi/angular/route/CustomerRoutes.kt
@@ -0,0 +1,39 @@
+package net.toshimichi.angular.route
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import net.toshimichi.angular.data.Customer
+import net.toshimichi.angular.service.CustomerService
+import org.koin.ktor.ext.inject
+
+fun Route.createCustomerRoutes() {
+
+    val customerService by inject<CustomerService>()
+
+    get("/customers") {
+        return@get call.respond(customerService.findAll())
+    }
+
+    get("/customers/{id}") {
+        val id = runCatching {
+            checkNotNull(call.parameters["id"]).toInt()
+        }.getOrElse {
+            return@get call.respondText("Invalid ID", status = HttpStatusCode.BadRequest)
+        }
+
+        val customer = runCatching {
+            checkNotNull(customerService.findById(id))
+        }.getOrElse {
+            return@get call.respondText("Could not find a customer", status = HttpStatusCode.BadRequest)
+        }
+
+        return@get call.respond(customer)
+    }
+
+    post<Customer>("/customers") {
+        customerService.addCustomer(it)
+        return@post call.respondText("", status = HttpStatusCode.OK)
+    }
+}
